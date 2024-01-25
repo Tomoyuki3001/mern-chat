770ed90d3b4e898b95e91a9892c88a97e6987079
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-24 17:06:08 -0800

    Added the plugin and created the login form

--- chat-clone/app/(site)/page.tsx
+++ chat-clone/app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -9,12 +10,13 @@ export default function Home() {
           height="48"
           width="48"
           className="mx-auto w-auto"
-          src="/Images/logo.png"
+          src="/images/logo.png"
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
       </div>
+      <AuthForm />
     </div>
   );
 }
