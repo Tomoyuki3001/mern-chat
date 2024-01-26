f003ed19333405e58899aa4b74907e150f751ede
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-26 14:37:32 -0800

    Created the first post request to check functions

--- chat-clone/app/(site)/components/AuthForm.tsx
+++ chat-clone/app/(site)/components/AuthForm.tsx
@@ -5,6 +5,7 @@ import Input from "../../components/inputs/Input";
 import Button from "@/app/components/Button";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -36,6 +37,7 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
+      axios.post("/api/register", data);
     }
     if (variant === "LOGIN") {
     }
